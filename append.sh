550aeb79d298eda22bea23fb150d25dc62c9e77d
Nicola Milani <nicoweb313@gmail.com>
2019-10-08 12:08:11 +0200

    add script require bootstrap

--- append.sh
+++ append.sh
@@ -14,6 +14,27 @@ set -f
 # END
 
 #done
+add_bootstrap_req(){
+for f in `find ./Classi -name "*.php"`;do
+ed $f << END
+20i
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+.
+w
+q
+END
+
+done
+
+}
 
 add_bootstrap(){
 for f in `find ./Classi -name "*.php"`;do
@@ -67,3 +88,12 @@ for f in `find ./Docenti -name "*.html"`;do
 
 done
 }
+
+do_replace_table_header(){
+for f in `find ./Classi -name "*.php"`;do
+
+sed -i -e 's/<TABLE BORDER=2 WIDTH="80%" CELLSPACING=0 CELLPADDING=4>/<TABLE CELLSPACING=0 style="float:center" class="table table-hover table-borderless table-responsive text-center">/g' ./Classi/*
+
+done
+}
+do_replace_table_header
